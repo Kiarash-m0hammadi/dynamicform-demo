5b56a8addb65a91ee9f46d4fbdab869ce1c27818
Kiarash-m0hammadi <kiarash7.mohammadi@gmail.com>
2024-09-08 16:16:57 +0330

    - App component to render a dynamic form based on the data provided in the dummy.json file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
+import DummyData from "./dummy.json";
 import "./App.css";
 
-function App() {
-  return <></>;
-}
+const infoArray = DummyData.info;
+
+const App = () => {
+  return (
+    <div>
+      <h1>Dynamic Form</h1>
+      <form>
+        {infoArray.map((field, id) => (
+          <div key={id}>
+            <label>{field.name}</label>
+            <input type="text" name={field.symbol} />
+          </div>
+        ))}
+      </form>
+    </div>
+  );
+};
 
 export default App;
