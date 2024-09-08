e796e4c5d6a7e114e89b683e5c6f3dbe855de227
Kiarash-m0hammadi <kiarash7.mohammadi@gmail.com>
2024-09-08 16:23:35 +0330

    add functionality to App component to render a dynamic form based on the data provided in the dummy.json file

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,34 @@
-import DummyData from "./dummy.json";
-import "./App.css";
-
-const infoArray = DummyData.info;
-
 const App = () => {
+  // Helper function to determine input type
+  const getInputField = (field) => {
+    switch (field.type) {
+      case "C":
+        return <input type="checkbox" name={field.symbol} />;
+      case "T":
+        return <input type="text" name={field.symbol} />;
+      case "N":
+        return <input type="number" name={field.symbol} />;
+      case "D":
+        return <input type="date" name={field.symbol} />;
+      case "U":
+        return <input type="file" name={field.symbol} />;
+      case "A":
+        return <textarea name={field.symbol} />;
+      default:
+        return <input type="text" name={field.symbol} />; // Default to text if unknown type
+    }
+  };
+
   return (
     <div>
       <h1>Dynamic Form</h1>
       <form>
-        {infoArray.map((field, id) => (
-          <div key={id}>
+        {infoArray.map((field, index) => (
+          <div key={index}>
+            {/* Render label */}
             <label>{field.name}</label>
-            <input type="text" name={field.symbol} />
+            {/* Render input field based on the type */}
+            {getInputField(field)}
           </div>
         ))}
       </form>
